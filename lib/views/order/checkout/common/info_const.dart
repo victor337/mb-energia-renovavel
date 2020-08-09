7158f8aa3a9ca856b3d2e61a665597e9b6ea6915
Victor <victorhspb17@gmail.com>
2020-08-09 04:48:48 -0300

    Alterações finais e finalização

--- lib/views/order/checkout/common/info_const.dart
+++ lib/views/order/checkout/common/info_const.dart
@@ -16,6 +16,8 @@ class InfoConst extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
       children: <Widget>[
         Icon(
           icon,
